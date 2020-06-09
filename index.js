3e0d11dc58f3022958001628ab75e8e2ffa03d67
amirhossein-mohammadpoor <mohammadpoor104@gmail.com>
2020-06-09 10:41:57 +0430

    map set center modified

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const WEATHER_API_KEY = "dc0b5da8c5660ba6d83ddfe07146c334"
 const MAPP_API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjJmYjZhOTdhOTZjMjkwMGE4ZWIyYzExOGY2NTU4MjAxY2ZiMzkwNDE0N2E3OTQ2ZDFlM2E5NjM2NjE2NDQ2ZGM5NzhkNjMzYTMyYzhiNGUzIn0.eyJhdWQiOiI5NTIwIiwianRpIjoiMmZiNmE5N2E5NmMyOTAwYThlYjJjMTE4ZjY1NTgyMDFjZmIzOTA0MTQ3YTc5NDZkMWUzYTk2MzY2MTY0NDZkYzk3OGQ2MzNhMzJjOGI0ZTMiLCJpYXQiOjE1OTE0NDA3NjEsIm5iZiI6MTU5MTQ0MDc2MSwiZXhwIjoxNTk0MDMyNzYxLCJzdWIiOiIiLCJzY29wZXMiOlsiYmFzaWMiXX0.RVfqLC_UwhYFsnvOqebOtAFkdtwGBWNOKRxIRsogWcX3twBEnxZDSE1bx4Dh8i-a7V_bITdD2N1U_PruPpj6Plr_gaqYfQtF-4FpaJuaxOlfGXM1YMXhvPYUdtnah1qGAzhza0RQOUYScoXDR5YJC6Jueq35ANFl2W7Cuw-WFLS-JnU2PvyMqe6ZNOzxKzyPqwG81ElZnvPkiuwwKiAhYLD5nSqiR3k-jYpj4CsIgYbaANd7mRMlM0B9VXxOl-RiCk4ucu8EBlPK7XAawmfJyW6up2crex_PRb4KKT5OpPKV8advzuPerHOfm_B4jm3f5zwkBXFL_IW0CLOltZyp2w'
 
+let app
+$("document").ready(function () {
+  app = new Mapp({
+    element: '#app',
+    presets: {
+      latlng: {
+        lat: 32,
+        lng: 52,
+      },
+      zoom: 4
+    },
+    apiKey:  MAPP_API_KEY
+  })
+  app.addLayers()
+  app.addZoomControls()
+  $(".mapp-logo").remove()
+})
+
 $.ajax({
   url: "https://restcountries.eu/rest/v2/all",
   method: "GET",
@@ -18,6 +36,7 @@ $("select").on("change", function (event) {
     url: `https://restcountries.eu/rest/v2/name/${$(this).val()}`
   })
     .done(country => {
+      
       $("[alt='flag']").attr("src", country[0].flag)
       $("#calling-code").text(country[0].callingCodes[0])
       $("#name").text(country[0].name)
@@ -27,6 +46,13 @@ $("select").on("change", function (event) {
       $("#population").text(country[0].population)
       $("#language").text(country[0].languages[0].name)
       $("#timezone").text(country[0].timezones[0])
+      app.addMarker({
+        name: 'basic-marker',
+        latlng: {
+          lat: country[0].latlng[0],
+          lng: country[0].latlng[1],
+        }
+      })
       $.ajax({
         url: `http://api.openweathermap.org/data/2.5/weather?q=${country[0].capital.replace(" ","+")}&appid=${WEATHER_API_KEY}&units=metric`
       })
@@ -40,20 +66,4 @@ $("select").on("change", function (event) {
           $(".over-layer").css("display", "none")
         })
     })
-})
-
-$("document").ready(function () {
-  let app = new Mapp({
-    element: '#app',
-    presets: {
-      latlng: {
-        lat: 32,
-        lng: 52,
-      },
-      zoom: 6
-    },
-    apiKey:  MAPP_API_KEY
-  })
-  app.addLayers()
-  app.addZoomControls()
 })
